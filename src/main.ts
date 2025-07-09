30d9b653e3019a5521a04222de9b308be475e146
kucing-mangap <arfakhsyadz.work@gmail.com>
2025-07-09 17:06:11 +0700

    scalar api setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Mini SIMRS')
+    .setDescription('The Mini SIMRS API Documentation')
+    .setVersion('0.1')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
+  app.use('/api-docs',
+    apiReference({
+      spec: {
+        content: document
+      }
+    })
+  )
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
