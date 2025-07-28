3e67ab846e455f45239aa3352bb80565175ad630
kucing-mangap <arfakhsyadz.work@gmail.com>
2025-07-28 21:35:17 +0700

    clean the project

--- prisma/migrations/20250709123815_make_phone_to_be_required_in_doctor_table/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `phone` on table `doctors` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "doctors" ALTER COLUMN "phone" SET NOT NULL;
--- src/medical-records/dto/update-medical-record.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateMedicalRecordDto } from './create-medical-record.dto';
-
-export class UpdateMedicalRecordDto extends PartialType(CreateMedicalRecordDto) {}
